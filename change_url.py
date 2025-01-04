b484e64a962a02eb98fbf3aef63ee1db1d5798fa
hwarange <asdf78964@naver.com>
2025-01-04 14:34:29 +0900

    chatchat

---
+++ change_url.py
@@ -0,0 +1,11 @@
+def change_url(url_id: str) -> str:
+    """
+    URL을 변경합니다.
+
+    Args:
+        url_id (str): 변경할 URL의 ID
+
+    Returns:
+        str: 변경된 URL
+    """
+    return f"https://www.rter2.com/hompyArticle/detail/{url_id}"
